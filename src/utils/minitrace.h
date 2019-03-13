e687737a35ca866336e949dd6012c9873587e1d7
Aurimas Blažulionis <0x60@pm.me>
2019-03-13 21:26:25 +0200

    Add missing file.

---
+++ src/utils/minitrace.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define MTR_SCOPED
+#include "../m0dular/submodules/minitrace/minitrace.h"
+#ifdef MTR_ENABLED
+#define MTR_SCOPED_TRACE(cat, name) auto scopedTrace = MTRScopedTrace(cat, name);
+#else
+#define MTR_SCOPED_TRACE(cat, name)
+#endif
